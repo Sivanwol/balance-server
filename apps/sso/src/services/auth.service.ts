838cc91532d4947f611e8f325cfd19bf78ab16c8
sivan wolberg <sivan@wolberg.pro>
2022-04-22 11:08:29 +0300

    refactor: remove all old logic of auth

--- apps/sso/src/services/auth.service.ts
+++ apps/sso/src/services/auth.service.ts
@@ -15,53 +15,8 @@ export class AuthService {
   readonly configService = Container.get( ConfigService ) as ConfigService;
   readonly usersService = Container.get( UsersService ) as UsersService;
 
-  public async login( userId: string, password: string ): Promise<UserModel> {
-
-    logger.info( "received service request => login" )
-    const findUser = await DbService.getInstance().connection
-      .user.findFirst( {
-        where: {
-          id: userId
-        },
-        include: {
-          userProfile: true
-        }
-      } )
-    if (!findUser) throw new UserNotFoundException( userId );
-
-    const isPasswordMatching: boolean = await bcrypt.compare( password, findUser.password );
-    if (!isPasswordMatching) throw new UserPasswordNotMatchedException( userId );
-
-    const user: UserModel = UserModel.toModel( findUser, (findUser?.userProfile || null) as UserProfile );
-    const settings = await this.configService.GetServiceSettingsByKey( 'users_auth_settings' ) as any
-    let hasNeedValidate = false;
-    if (settings) {
-      if (settings.revalidate_two_way_auth && user.verifiedAccessPeriodDiff >= settings.revalidate_two_way_auth_period){
-        user.requiredValidatedLogin = true;
-        hasNeedValidate = true;
-      }
-    }
-    if (!hasNeedValidate) {
-      user.access_token = this.createToken( userId )
-    }
-    return user;
-  }
-
   public registerUser(user: RegisterUserDto) {
     return;
-  }
-
-  public createToken( userId: string ): TokenData {
-    const dataStoredInToken: DataStoredInToken = {user_id: userId};
-    const secretKey: string = process.env.SECRET as string;
-    const expiresIn: number = 60 * parseInt( process.env.JWT_EXPIRE || '60' );
-    return {
-      expiresIn, token: jwt.sign( dataStoredInToken, secretKey, {
-        issuer: process.env.ISSUER,
-        audience: process.env.Audience,
-        expiresIn
-      } )
-    };
   }
 }
 
