e63dd6232b76bd7af2e721ff7168f1dbe80949b8
sivan wolberg <sivan@wolberg.pro>
2022-04-23 21:08:25 +0300

    refactor: refactor users  service

--- libs/utils-server/src/services/users.service.ts
+++ libs/utils-server/src/services/users.service.ts
@@ -2,7 +2,14 @@ import { RegisterUserAuth0Dto } from '@balancer/utils-server/dtos/users.dto';
 import { User } from '@prisma/client';
 import { v5 as uuidv5 } from 'uuid';
 import * as bcrypt from 'bcrypt';
-import { EmailService, RolesService, PermissionsService, DbService, ActivityLogService, ActivityType } from './';
+import {
+  EmailService,
+  RolesService,
+  PermissionsService,
+  DbService,
+  ActivityLogService,
+  ActivityType,
+} from './';
 import { Container, Service } from 'typedi';
 import { UserModel } from '../models/user.model';
 import { UserNotFoundException } from '../exceptions/UserNotFoundException';
@@ -12,115 +19,124 @@ import { CacheKeys } from '../constraints/CacheKeys';
 
 @Service()
 export class UsersService {
+  readonly emailService = Container.get(EmailService) as EmailService;
+  readonly rolesService = Container.get(RolesService) as RolesService;
+  readonly permissionsService = Container.get(
+    PermissionsService
+  ) as PermissionsService;
+  readonly activityLogService = Container.get(
+    ActivityLogService
+  ) as ActivityLogService;
 
-  readonly emailService = Container.get( EmailService ) as EmailService;
-  readonly rolesService = Container.get( RolesService ) as RolesService;
-  readonly permissionsService = Container.get( PermissionsService ) as PermissionsService;
-  readonly activityLogService = Container.get( ActivityLogService ) as ActivityLogService;
-
-
-  public async getUserModelById( userId: string ): Promise<User> {
-    logger.info( "received service request => getUserModelById" )
-    const locateUser = await DbService.getInstance().connection.user
-      .findFirst( {
-        where: {
-          id: userId
-        }
-      } );
-    if (!locateUser) throw new UserNotFoundException( userId );
-    return locateUser
+  public async getUserModelById(userId: string): Promise<User> {
+    logger.info('received service request => getUserModelById');
+    const locateUser = await DbService.getInstance().connection.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+    if (!locateUser) throw new UserNotFoundException(userId);
+    return locateUser;
   }
 
-  public async getUserModelByEmail( email: string ): Promise<User> {
-    logger.info( "received service request => getUserModelByEmail" )
-    const locateUser = await DbService.getInstance().connection.user
-      .findFirst( {
-        where: {
-          email
-        }
-      } );
-    if (!locateUser) throw new UserNotFoundException( email );
-    return locateUser
+  public async getUserModelByEmail(email: string): Promise<User> {
+    logger.info('received service request => getUserModelByEmail');
+    const locateUser = await DbService.getInstance().connection.user.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (!locateUser) throw new UserNotFoundException(email);
+    return locateUser;
   }
 
-  public async findAllUser(returnModels: boolean): Promise<UserModel[]| User[]> {
-    logger.info( "received service request => findAllUser" )
-    const users = await DbService.getInstance().connection.user
-      .findMany();
-    return (returnModels)? users.map( u => UserModel.toModel( u )): users;
+  public async findAllUser(
+    returnModels: boolean
+  ): Promise<UserModel[] | User[]> {
+    logger.info('received service request => findAllUser');
+    const users = await DbService.getInstance().connection.user.findMany();
+    return returnModels ? users.map((u) => UserModel.toModel(u)) : users;
   }
 
-  public async findUserById( userId: string, notFilters?: boolean ): Promise<UserModel | null> {
-    logger.info( "received service request => findUserById" )
-    const cacheKey = CacheKeys.LocateUser( userId )
+  public async findUserById(
+    userId: string,
+    notFilters?: boolean
+  ): Promise<UserModel | null> {
+    logger.info('received service request => findUserById');
+    const cacheKey = CacheKeys.LocateUser(userId);
     let locateUser;
     if (!notFilters) {
-      const status = await DbService.getInstance().HasCache( cacheKey )
+      const status = await DbService.getInstance().HasCache(cacheKey);
       if (!status) {
-        locateUser = await DbService.getInstance().connection.user
-          .findFirst( {
-            where: {
-              id: userId,
-              disabledAt: null,
-              disabledForeverAt: null,
-              NOT: {
-                emailVerified: null
-              }
-            }} );
+        locateUser = await DbService.getInstance().connection.user.findFirst({
+          where: {
+            id: userId,
+            disabledAt: null,
+            NOT: {
+              emailVerified: null,
+            },
+          },
+        });
         if (!locateUser) return null;
-        await DbService.getInstance().CacheResult<User>( cacheKey, locateUser );
+        await DbService.getInstance().CacheResult<User>(cacheKey, locateUser);
       } else {
-        locateUser = await DbService.getInstance().GetCacheQuery( cacheKey ) as User
+        locateUser = (await DbService.getInstance().GetCacheQuery(
+          cacheKey
+        )) as User;
       }
     } else {
-      locateUser = await DbService.getInstance().connection.user
-        .findFirst( {
-          where: {
-            id: userId
-          }
-        } );
-      if (!locateUser) throw new UserNotFoundException( userId );
+      locateUser = await DbService.getInstance().connection.user.findFirst({
+        where: {
+          id: userId,
+        },
+      });
+      if (!locateUser) throw new UserNotFoundException(userId);
     }
-    return UserModel.toModel( locateUser );
+    return UserModel.toModel(locateUser);
   }
 
-  public async findUserByAuth0UserId( userId: string, notFilters?: boolean ): Promise<UserModel | null> {
-    logger.info( "received service request => findUserById" )
-    const cacheKey = CacheKeys.LocateUser( userId )
+  public async findUserByAuth0UserId(
+    userId: string,
+    notFilters?: boolean
+  ): Promise<UserModel | null> {
+    logger.info('received service request => findUserById');
+    const cacheKey = CacheKeys.LocateUser(userId);
     let locateUser;
     if (!notFilters) {
-      const status = await DbService.getInstance().HasCache( cacheKey )
+      const status = await DbService.getInstance().HasCache(cacheKey);
       if (!status) {
-        locateUser = await DbService.getInstance().connection.user
-          .findFirst( {
-            where: {
-              authUserId: userId,
-              disabledAt: null,
-              disabledForeverAt: null,
-              NOT: {
-                emailVerified: null
-              }
-            }
-          } );
+        locateUser = await DbService.getInstance().connection.user.findFirst({
+          where: {
+            authUserId: userId,
+            disabledAt: null,
+            NOT: {
+              emailVerified: null,
+            },
+          },
+        });
         if (!locateUser) return null;
-        await DbService.getInstance().CacheResult<User>( cacheKey, locateUser );
+        await DbService.getInstance().CacheResult<User>(cacheKey, locateUser);
       } else {
-        locateUser = await DbService.getInstance().GetCacheQuery( cacheKey ) as User
+        locateUser = (await DbService.getInstance().GetCacheQuery(
+          cacheKey
+        )) as User;
       }
     } else {
-      locateUser = await DbService.getInstance().connection.user
-        .findFirst( {
-          where: {
-            id: userId
-          } );
-      if (!locateUser) throw new UserNotFoundException( userId );
+      locateUser = await DbService.getInstance().connection.user.findFirst({
+        where: {
+          id: userId,
+        },
+      });
+      if (!locateUser) throw new UserNotFoundException(userId);
     }
-    return UserModel.toModel( locateUser);
+    return UserModel.toModel(locateUser);
   }
 
-  public async registerUser( registerUserDto: RegisterUserAuth0Dto ): Promise<UserModel | null> {
-    logger.info( "received service request => registerUser" )
-    const locateUser = await this.findUserByEmail( registerUserDto.email, true )
+  public async registerUser(
+    registerUserDto: RegisterUserAuth0Dto
+  ): Promise<UserModel | null> {
+    logger.info('received service request => registerUser');
+    const locateUser = await this.findUserByEmail(registerUserDto.email, true);
     let record;
     const user = {
       authUserId: registerUserDto.userId,
@@ -128,51 +144,42 @@ export class UsersService {
       emailVerified: registerUserDto.verifyEmail,
       mobile: registerUserDto.phone,
       mobileVerified: registerUserDto.verifyPhone,
-      disabledAt: null
-    }
+      disabledAt: null,
+    };
     if (!locateUser) {
-      record = await DbService.getInstance().connection.user.create( {
-        data: user
-      } )
+      record = await DbService.getInstance().connection.user.create({
+        data: user,
+      });
     }
-    const userId = record.id || locateUser.id
-    logger.info( `Created user with id: ${userId}` )
-    return await this.findUserById( userId, true )
+    const userId = record.id || locateUser.id;
+    logger.info(`Created user with id: ${userId}`);
+    return await this.findUserById(userId, true);
   }
 
-  public async findUserByEmail( email: string, noFilters?: boolean ): Promise<UserModel | null> {
-    logger.info( "received service request => findUserByEmail" )
+  public async findUserByEmail(
+    email: string,
+    noFilters?: boolean
+  ): Promise<UserModel | null> {
+    logger.info('received service request => findUserByEmail');
     let locateUser;
     if (!noFilters) {
-      locateUser = await DbService.getInstance().connection.user
-        .findFirst( {
-          where: {
-            email: email,
-            disabledAt: null,
-            disabledForeverAt: null,
-            NOT: {
-              emailVerified: null
-            }
+      locateUser = await DbService.getInstance().connection.user.findFirst({
+        where: {
+          email: email,
+          disabledAt: null,
+          NOT: {
+            emailVerified: null,
           },
-          include: {
-            userProfile: true,
-            access: true
-          }
-        } );
+        },
+      });
     } else {
-      locateUser = await DbService.getInstance().connection.user
-        .findFirst( {
-          where: {
-            email: email,
-          },
-          include: {
-            userProfile: true,
-            access: true
-          }
-        } );
+      locateUser = await DbService.getInstance().connection.user.findFirst({
+        where: {
+          email: email,
+        },
+      });
     }
     if (!locateUser) return null;
-    return UserModel.toModel( locateUser, locateUser.userProfile );
+    return UserModel.toModel(locateUser);
   }
-
 }
