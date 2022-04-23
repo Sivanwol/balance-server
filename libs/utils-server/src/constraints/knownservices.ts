e55de217102661db8d88fa5df225ebfbfb75ef56
sivan wolberg <sivan@wolberg.pro>
2022-04-23 21:28:24 +0300

    refactor: update the services existed in system

--- libs/utils-server/src/constraints/knownservices.ts
+++ libs/utils-server/src/constraints/knownservices.ts
@@ -1,12 +1,7 @@
 export const knownServices = [
   'Web-API-Gateway',
-  'Backend',
   'ConfigService',
   'SSO-server',
-  'Users',
-  'Communities',
-  'Communities-Groups',
-  'Payments'
 ]
 
 export enum PlatformStatus {
@@ -17,12 +12,7 @@ export enum PlatformStatus {
 export enum ServicesRoute {
   APIGateWay = 'Web-API-Gateway',
   SSO = "SSO-server",
-  Backend = "Backend",
-  Users = "Users",
-  Communities = "Communities",
   ConfigService = "ConfigService",
-  CommunitiesGroups = "Communities-Groups",
-  Payments = "Payments",
 }
 
 export const SharedServicesEvents = [
