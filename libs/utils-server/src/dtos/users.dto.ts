18d8daaabbce8603fca11253e112cf7d155188e3
sivan wolberg <sivan@wolberg.pro>
2022-05-05 23:21:22 +0300

    refactor: fix issue with user register

--- libs/utils-server/src/dtos/users.dto.ts
+++ libs/utils-server/src/dtos/users.dto.ts
@@ -1,4 +1,13 @@
-import { IsEmail, IsString, IsOptional, MinLength, MaxLength, Matches, IsBoolean } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  IsOptional,
+  MinLength,
+  MaxLength,
+  Matches,
+  IsBoolean,
+  IsMobilePhone,
+} from 'class-validator';
 
 /**
  * Auth0 user model
@@ -14,8 +23,8 @@ import { IsEmail, IsString, IsOptional, MinLength, MaxLength, Matches, IsBoolean
 export class RegisterUserAuth0Dto {
   @IsString()
   @IsOptional()
-  @MinLength( 8 )
-  @MaxLength( 32 )
+  @MinLength(8)
+  @MaxLength(32)
   public userId: string;
   @IsString()
   public userName: string;
@@ -24,10 +33,13 @@ export class RegisterUserAuth0Dto {
   public displayName: string;
   @IsEmail()
   public email: string;
+  @IsOptional()
   @IsBoolean()
   public verifyEmail: boolean;
-  @IsEmail()
+  @IsMobilePhone()
+  @IsOptional()
   public phone: string;
+  @IsOptional()
   @IsBoolean()
   public verifyPhone: boolean;
 }
@@ -35,17 +47,21 @@ export class LoginUserDto {
   @IsEmail()
   public email: string;
 
-  @MinLength( 8 )
-  @MaxLength( 32 )
-  @Matches( /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'password too weak'} )
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   @IsString()
   public password: string;
 }
 
 export class ForgetPasswordUserDto {
-  @MinLength( 8 )
-  @MaxLength( 32 )
-  @Matches( /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'password too weak'} )
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   @IsString()
   public password: string;
 }
@@ -54,20 +70,22 @@ export class RegisterUserDto {
   @IsEmail()
   public email: string;
 
-  @MinLength( 8 )
-  @MaxLength( 32 )
-  @Matches( /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'password too weak'} )
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   @IsString()
   public password: string;
 
   @IsOptional()
   @IsString()
-  @MinLength( 2 )
-  @MaxLength( 40 )
+  @MinLength(2)
+  @MaxLength(40)
   public firstName: string;
   @IsOptional()
   @IsString()
-  @MinLength( 2 )
-  @MaxLength( 40 )
+  @MinLength(2)
+  @MaxLength(40)
   public lastName: string;
 }
