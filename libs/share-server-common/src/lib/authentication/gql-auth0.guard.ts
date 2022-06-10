ad411b3e6f8a4e5155381b87789e47c8769031c1
sivan wolberg <sivan@wolberg.pro>
2022-06-11 00:29:24 +0300

    fix: now align with gql

--- libs/share-server-common/src/lib/authentication/gql-auth0.guard.ts
+++ libs/share-server-common/src/lib/authentication/gql-auth0.guard.ts
@@ -2,16 +2,15 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { UnauthorizedException } from '..';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 
 @Injectable()
 export class GqlAuth0Guard extends AuthGuard('auth0') {
   canActivate(context: ExecutionContext) {
     try {
       const graphqlContext = GqlExecutionContext.create(context);
-      const [_, token] = graphqlContext.getContext().req.headers.authorization.split(' ');
-
-      console.log(graphqlContext.getContext());
-      if (token) return true;
+      const { req } = graphqlContext.getContext();
+      return super.canActivate(new ExecutionContextHost([req]));
     } catch (error) {
       throw new UnauthorizedException();
     }
